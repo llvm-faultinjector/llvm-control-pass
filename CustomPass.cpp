8098f9235bcb0e723c60814d9bd6c7cdbe3dbe04
rollrat <rollrat@naver.com>
2017-12-26 19:56:25 +0900

    Add: Interprocedure dependency check routine.

--- CustomPass.cpp
+++ CustomPass.cpp
@@ -23,17 +23,26 @@ namespace {
 	{
 		errs() << "\n";
 		errs() << "Function " << F.getName() +"\n";
-		std::vector<Value *> v = findAnnotation(F);
-		for (auto s : v)
-			findDependency(s, F);
+		//std::vector<Value *> v = findAnnotation(F);
+		//for (auto s : v)
+		//	findDependency(s, F);
+		//std::vector<Argument *> args = findFunctionArgumentReturnDependency(F);
+		//for ( auto arg : args )
+		//    errs() << " - Argument \"" << arg->getName() << "\" affect the return value.\n";
+		findFunctionCallByReferenceDependency(F);
 		errs() << "\n";
 		return false;
 	}
+	
+	// get annotated message
+	// int __attribute__((annotate("xxx"))) a; => "xxx"
 	StringRef getAnnotatedString(CallInst *c)
 	{
 		Constant *cs = (cast<GlobalVariable>((cast<ConstantExpr>(c->getArgOperand(1)))->getOperand(0)))->getInitializer();
 		return cast<ConstantDataArray>(cs)->getAsCString();
 	}
+	
+	// find all annotated valuable
 	std::vector<Value *> findAnnotation(Function &F)
 	{
 		std::vector<Value *> annotate;
@@ -82,12 +91,12 @@ namespace {
 		}
 		errs() << "\n   * follow nodes\n";
 		for ( auto& v : step )
-			printFollowNodes(v);
+			printFollowNodes(F, v);
 		errs() << "\n";
 		dependency_map.insert(std::pair<Value *, std::vector<Instruction *>>(V, dependency));
 		dependency.clear();
 	}
-	void printFollowNodes(Value *X)
+	void printFollowNodes(Function &F, Value *X)
 	{
 		if (Instruction *inst = dyn_cast<Instruction> (X))
 		{
@@ -101,18 +110,127 @@ namespace {
 			if ( PHINode *phi = dyn_cast<PHINode> (inst) )
 			{
 				for (unsigned i = 0; i < phi->getNumIncomingValues(); i++)
-					printFollowNodes(phi->getIncomingValue(i));
+				{
+					printFollowNodes(F, phi->getIncomingValue(i));
+					printFollowNodesByStoreInst(F, phi->getIncomingValue(i));
+				}
 			}
 			else
 			{			
 				for (unsigned i = 0; i < inst->getNumOperands(); i++)
-					printFollowNodes(inst->getOperand(i));
+				{
+					printFollowNodes(F, inst->getOperand(i));
+					printFollowNodesByStoreInst(F, phi->getOperand(i));
+				}
 			}
 		}
+	}
+	void printFollowNodesByStoreInst(Function &F, Value *v)
+	{
+		for ( auto& bb : F )
+		{
+			for ( auto& i : bb )
+			{
+				if (StoreInst* si = dyn_cast<StoreInst> (&i))
+				{
+					if (si->getPointerOperand() == v)
+					{
+					    printFollowNodes(F, si);
+					}
+				}
+			}
+		}
+	}
+	
+	// Checks which arguments affect the return value of a function.
+	std::vector<Argument *> findFunctionArgumentReturnDependency(Function &F)
+	{
+	    std::vector<Argument *> vec;
+	    if (F.doesNotReturn())
+	        return vec; 
+	    std::vector<Argument *> args;
+	    for (auto arg = F.arg_begin(); arg != F.arg_end(); arg++)
+	        args.push_back(arg);
+		for ( auto& bb : F )
+		{
+			for ( auto& i : bb )
+			{
+			    if (ReturnInst *RI = dyn_cast<ReturnInst>(&i))
+			    {
+			        for ( auto arg : args )
+			        {
+			            if (_InternalfindFunctionArgumentReturnDependency(F, RI->getOperand(0), arg))
+			                vec.push_back(arg);
+			        }
+			    }
+			}
+		}
+	    return vec;
+	}
+	bool _InternalfindFunctionArgumentReturnDependency(Function &F, Value *v, Argument *arg)
+	{
+	    if (v == arg) return true;
+		if (Instruction *inst = dyn_cast<Instruction> (v))
+		{
+			if ( PHINode *phi = dyn_cast<PHINode> (inst) )
+			{
+				for (unsigned i = 0; i < phi->getNumIncomingValues(); i++)
+				{
+					if (_InternalfindFunctionArgumentReturnDependency(F, phi->getIncomingValue(i), arg) ||
+					    _InternalfindFunctionArgumentReturnDependencyByStoreInst(F, phi->getIncomingValue(i), arg))
+					    return true;
+			    }
+			}
+			else
+			{
+				for (unsigned i = 0; i < inst->getNumOperands(); i++)
+				{
+					if (_InternalfindFunctionArgumentReturnDependency(F, inst->getOperand(i), arg) ||
+					    _InternalfindFunctionArgumentReturnDependencyByStoreInst(F, inst->getOperand(i), arg))
+					    return true;
+			    }
+			}
+		}
+		return false;
+	}
+	bool _InternalfindFunctionArgumentReturnDependencyByStoreInst(Function &F, Value *v, Argument *arg)
+	{
+		for ( auto& bb : F )
+		{
+			for ( auto& i : bb )
+			{
+				if (StoreInst* si = dyn_cast<StoreInst> (&i))
+				{
+					if (si->getPointerOperand() == v)
+					{
+					    if (_InternalfindFunctionArgumentReturnDependency(F, si->getValueOperand(), arg))
+					        return true;
+					}
+				}
+			}
+		}
+		return false;
+	}
+	
+	std::vector<Argument *> findFunctionCallByReferenceDependency(Function &F)
+	{
+	    std::vector<Argument *> vec;
+	    // find dereferenceable variable
+	    std::vector<Argument *> args;
+	    for (auto arg = F.arg_begin(); arg != F.arg_end(); arg++)
+	    {
+	        if (arg->getType()->isPointerTy())
+	            if (arg->getDereferenceableBytes() > 0)
+	            {
+	                errs() << " - derefer: " << arg->getName() << "\n";
+	                args.push_back(arg);
+	            }
+	    }
+	    return vec;
 	}
 
   };
 }
 
 char CustomPass::ID = 0;
-static RegisterPass<CustomPass> X("custom", "Hello World Pass");
+static RegisterPass<CustomPass> X("custom", "CustomPass");
